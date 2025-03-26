e15f2a401be5ef1ba3f66054bf659be3a8b943a9
pwendell25 <apaulwendell256@gmail.com>
2025-03-27 02:54:51 +0800

    first commit

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { Button } from '@/Components/ui/button'
+</script>
+
+<template>
+
+  <div class="ml-10 mt-10">
+    <Button class="bg-blue-300 hover:bg-blue-300/80 ease-in-out transition-all duration-300">Click me</Button>
+  </div>
+</template>
