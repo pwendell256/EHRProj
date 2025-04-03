e1a51bf4fc9cfdecb350d2d77fa5168245ae29ee
pwendell25 <apaulwendell256@gmail.com>
2025-04-03 12:55:48 +0800

    commit for pull

--- resources/js/Layouts/tabs/MedicalRecordTab/Imaging.vue
+++ resources/js/Layouts/tabs/MedicalRecordTab/Imaging.vue
@@ -18,13 +18,13 @@
             <p v-if="form.errors.dateTime" class="text-red-500 text-sm">{{ form.errors.dateTime }}</p>
           </div>
 
-          <div class="flex flex-col gap-2 md:col-span-2">
+          <div class="flex flex-col gap-2col-span-2">
             <Label for="result">Result</Label>
             <Input id="result" v-model="form.result" type="text" required />
             <p v-if="form.errors.result" class="text-red-500 text-sm">{{ form.errors.result }}</p>
           </div>
 
-          <div class="flex flex-col gap-2 md:col-span-2">
+          <div class="flex flex-col gap-2 col-span-2">
             <Label for="image">Upload Image</Label>
             <input id="image" type="file" @change="handleImageUpload" accept="image/*"
               class="w-full border rounded px-3 py-2" />
@@ -211,7 +211,7 @@ import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import { router, useForm, usePage } from '@inertiajs/vue3';
 import { Link } from '@inertiajs/vue3';
 
-import { Button } from '@/Components/ui/button'
+import { Button } from '@/components/ui/button'
 import {
   Dialog,
   DialogContent,
